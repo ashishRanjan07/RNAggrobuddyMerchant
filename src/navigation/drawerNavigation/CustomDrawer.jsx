38f5b2d24371b0510e32f8b8df13fcbca8e1b033
ashishranjan9629 <ashish.ranjan@velocis.co.in>
2024-08-06 17:50:07 +0530

    Added Custom Drawer Section and their design

---
+++ src/navigation/drawerNavigation/CustomDrawer.jsx
@@ -0,0 +1,128 @@
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {DrawerItemList} from '@react-navigation/drawer';
+import {AppColor} from '../../theme/AppColor';
+import {responsive} from '../../utils/Responsive';
+import {ImagePath} from '../../utils/ImagePath';
+import DeviceInfo from 'react-native-device-info';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+const CustomDrawer = props => {
+  return (
+    <View style={styles.drawerContent}>
+      {/* Upper Head */}
+      <View style={styles.upperSection}>
+        <Image
+          source={ImagePath.logo}
+          resizeMode="cover"
+          style={styles.imageStyle}
+        />
+        <Text
+          style={styles.text}>{`Welcome \n to \n Aggrobuddy Worker App`}</Text>
+      </View>
+      <View style={styles.versionSection}>
+        <Text style={styles.versionText}>
+          App Version : {DeviceInfo.getVersion()}
+        </Text>
+        <TouchableOpacity>
+          <MaterialIcons
+            name="browser-updated"
+            color={AppColor.C4}
+            size={responsive(30)}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <DrawerItemList {...props} />
+      <View style={styles.socialMediaHolder}>
+      <MaterialCommunityIcons
+          name="whatsapp"
+          size={responsive(40)}
+          color={AppColor.C4}
+        />
+         <MaterialCommunityIcons
+          name="linkedin"
+          size={responsive(40)}
+          color={AppColor.C4}
+        />
+        <MaterialCommunityIcons
+          name="facebook"
+          size={responsive(40)}
+          color={AppColor.C4}
+        />
+        <MaterialCommunityIcons
+          name="instagram"
+          size={responsive(40)}
+          color={AppColor.C4}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default CustomDrawer;
+
+const styles = StyleSheet.create({
+  drawerContent: {
+    flex: 1,
+    backgroundColor: AppColor.background,
+  },
+  upperSection: {
+    padding: responsive(15),
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  imageStyle: {
+    height: responsive(150),
+    width: responsive(100),
+  },
+  text: {
+    width: '60%',
+    lineHeight: responsive(30),
+    letterSpacing: responsive(1),
+    fontSize: responsive(18),
+    color: AppColor.C4,
+    fontFamily: 'NotoSans-Medium',
+    textAlign: 'center',
+  },
+  versionText: {
+    fontSize: responsive(16),
+    textAlign: 'center',
+    color: AppColor.C4,
+    fontFamily: 'NotoSans-Medium',
+    borderColor: AppColor.C4,
+  },
+  versionSection: {
+    flexDirection: 'row',
+    borderWidth: 2,
+    marginBottom: responsive(10),
+    padding: responsive(10),
+    alignItems: 'center',
+    borderColor: AppColor.C4,
+    justifyContent: 'space-evenly',
+  },
+  bottomHolder: {
+    borderWidth: 2,
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    padding: responsive(10),
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  socialMediaHolder: {
+    borderWidth: 2,
+    borderColor: AppColor.white,
+    position: 'absolute',
+    padding: responsive(10),
+    borderRadius: responsive(5),
+    gap: responsive(10),
+    alignItems: 'center',
+    bottom: 0,
+    width: '100%',
+    backgroundColor: AppColor.white,
+    elevation: responsive(10),
+    flexDirection:'row',
+    justifyContent:'space-evenly'
+  },
+});
--- src/navigation/drawerNavigation/DrawerNavigation.jsx
+++ src/navigation/drawerNavigation/DrawerNavigation.jsx
@@ -1,23 +1,136 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import BottomNavigation from '../bottomNavigation/BottomNavigation';
 import SavePost from '../../screens/appScreen/SavePost';
-import Demo from '../../screens/appScreen/Demo';
+import CustomDrawer from './CustomDrawer';
+import Feather from 'react-native-vector-icons/Feather';
+import {responsive} from '../../utils/Responsive';
+import {AppColor} from '../../theme/AppColor';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import PrivacyPolicy from '../../screens/appScreen/PrivacyPolicy';
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigation = () => {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name='DashBoard' component={BottomNavigation} options={{headerShown:false}}/>
-      <Drawer.Screen name = 'Save Feed' component={SavePost} options={{headerShown:false}}/>
-      <Drawer.Screen name='Demo' component={Demo} options={{headerShown:true}}/>
+    <Drawer.Navigator drawerContent={props => <CustomDrawer {...props} />}>
+      <Drawer.Screen
+        name="DashBoard"
+        component={BottomNavigation}
+        options={{
+          headerShown: false,
+          drawerIcon: ({color}) => (
+            <MaterialIcons
+              name="dashboard"
+              size={responsive(30)}
+              color={AppColor.C4}
+            />
+          ),
+          drawerLabelStyle: styles.labelStyle,
+          drawerAllowFontScaling: false,
+        }}
+      />
+      <Drawer.Screen
+        name="Save Post"
+        component={SavePost}
+        options={{
+          headerShown: false,
+          drawerIcon: ({color}) => (
+            <MaterialIcons
+              name="save-alt"
+              size={responsive(30)}
+              color={AppColor.C4}
+            />
+          ),
+          drawerLabelStyle: styles.labelStyle,
+          drawerAllowFontScaling: false,
+        }}
+      />
+      <Drawer.Screen
+        name="About us"
+        component={SavePost}
+        options={{
+          headerShown: false,
+          drawerIcon: ({color}) => (
+            <Feather name="users" size={responsive(30)} color={AppColor.C4} />
+          ),
+          drawerLabelStyle: styles.labelStyle,
+          drawerAllowFontScaling: false,
+        }}
+      />
+      <Drawer.Screen
+        name="Share App"
+        component={SavePost}
+        options={{
+          headerShown: false,
+          drawerIcon: ({color}) => (
+            <Feather name="share" size={responsive(30)} color={AppColor.C4} />
+          ),
+          drawerLabelStyle: styles.labelStyle,
+          drawerAllowFontScaling: false,
+        }}
+      />
+      <Drawer.Screen
+        name="Privacy Policy"
+        component={PrivacyPolicy}
+        options={{
+          headerShown: false,
+          drawerIcon: ({color}) => (
+            <MaterialIcons
+              name="policy"
+              size={responsive(30)}
+              color={AppColor.C4}
+            />
+          ),
+          drawerLabelStyle: styles.labelStyle,
+          drawerAllowFontScaling: false,
+        }}
+      />
+
+      <Drawer.Screen
+        name="Change Theme"
+        component={SavePost}
+        options={{
+          headerShown: false,
+          drawerIcon: ({color}) => (
+            <MaterialIcons
+              name="dark-mode"
+              size={responsive(30)}
+              color={AppColor.C4}
+            />
+          ),
+          drawerLabelStyle: styles.labelStyle,
+          drawerAllowFontScaling: false,
+        }}
+      />
+            <Drawer.Screen
+        name="Help"
+        component={SavePost}
+        options={{
+          headerShown: false,
+          drawerIcon: ({color}) => (
+            <MaterialIcons
+              name="help-center"
+              size={responsive(30)}
+              color={AppColor.C4}
+            />
+          ),
+          drawerLabelStyle: styles.labelStyle,
+          drawerAllowFontScaling: false,
+        }}
+      />
     </Drawer.Navigator>
-  )
-}
+  );
+};
 
-export default DrawerNavigation
+export default DrawerNavigation;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  labelStyle: {
+    color: AppColor.C4,
+    fontFamily: 'NotoSans-Medium',
+    fontSize: responsive(18),
+  },
+});
